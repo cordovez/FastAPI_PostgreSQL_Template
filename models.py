9441bf3ee8d4d8c434ab272ae960510e743b00e7
Juan Carlos Cordovez-Mantilla <cordovez@el-cordovez.com>
2024-02-20 14:56:49 +0100

    New Person Class and User routes

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
+from enum import Enum, auto, StrEnum
+from sqlalchemy import Column, String
+from pydantic import BaseModel, EmailStr
 
 
 class BookBase(SQLModel):
@@ -21,8 +24,137 @@ class BookRead(BookBase):
     id: int
 
 
-class BookUpdate(SQLModel):
+class BookUpdate(BookCreate):
     title: Optional[str] = None
     author: Optional[str] = None
     description: Optional[str] = None
     password: Optional[str] = None
+    editor: Optional[str] = None
+    isbn: Optional[str] = None
+    language: Optional[str] = None
+
+
+class TeamBase(SQLModel):
+    name: str = Field(index=True)
+    headquarters: str
+
+
+"""
+TEAMS
+"""
+
+
+class Team(TeamBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    heroes: list["Hero"] = Relationship(back_populates="team")
+
+
+class TeamCreate(TeamBase):
+    pass
+
+
+class TeamRead(TeamBase):
+    id: int
+
+
+class TeamUpdate(SQLModel):
+    name: Optional[str] = None
+    headquarters: Optional[str] = None
+
+
+"""
+Persons
+"""
+
+
+class Role(StrEnum):
+    ACADEMIC_ADMIN = auto()
+    TECH_ADMIN = auto()
+    INSTRUCTOR = auto()
+    STUDENT = auto()
+    STAFF = auto()
+
+
+class PersonBase(SQLModel):
+    username: str = Field(index=True)
+    email: str = Field(index=True)
+    first_name: str
+    middle_name: Optional[str] = Field(default=None)
+    last_name: str = Field(index=True)
+    role: str
+    primary_contact: Optional[str] = Field(default=None)
+    secondary_contact: Optional[str] = Field(default=None)
+    street_address: Optional[str] = Field(default=None)
+    post_code: Optional[str] = Field(default=None)
+    locality: Optional[str] = Field(default=None)
+    country: Optional[str] = Field(default=None)
+
+
+class PersonDB(PersonBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    password_hashed: Optional[str] = Field(default=None)
+
+
+class PersonRead(PersonBase):
+    id: int
+
+
+class PersonCreate(SQLModel):
+    email: EmailStr
+    first_name: str
+    last_name: str
+    password: str
+
+
+class PersonUpdate(SQLModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    first_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    last_name: Optional[str] = None
+    primary_contact: Optional[str] = None
+    secondary_contact: Optional[str] = None
+    street_address: Optional[str] = None
+    post_code: Optional[str] = None
+    locality: Optional[str] = None
+    country: Optional[str] = None
+
+
+class Login(SQLModel):
+    username: str
+    password: str
+
+
+"""
+Heroes
+"""
+
+
+class HeroBase(SQLModel):
+    name: str = Field(index=True)
+    secret_name: str
+    age: Optional[int] = Field(default=None, index=True)
+
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+
+
+class Hero(HeroBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    team: Optional[Team] = Relationship(back_populates="heroes")
+
+
+class HeroRead(HeroBase):
+    id: int
+
+
+class HeroCreate(HeroBase):
+    pass
+
+
+class HeroUpdate(SQLModel):
+    name: Optional[str] = None
+    secret_name: Optional[str] = None
+    age: Optional[int] = None
+    team_id: Optional[int] = None
--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,21 +1,126 @@
-from sqlmodel import Session, select
-
-# from sqlalchemy import select
-from fastapi import Depends, HTTPException, APIRouter, status
-
-# from typing import Annotated
+from fastapi import Depends, HTTPException, APIRouter, status, Query
 from db.database import engine
+from models import PersonCreate, PersonRead, PersonUpdate, PersonDB, Role
+from sqlmodel import select, Session
 
 user_router = APIRouter()
 
 
-@user_router.post(
-    "/add",
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+def create_default_username(fname, lname):
+    return (fname[0] + lname[:7]).lower()
+
+
+def hash_password(password: str) -> str:
+    return f"faked {password} goes here"
+
+
+@user_router.get(
+    "/all", response_model=list[PersonRead], status_code=status.HTTP_200_OK
 )
-async def add_person(person):
-    pass
+async def get_all_users(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    return session.exec(select(PersonDB).offset(offset).limit(limit)).all()
 
 
-@user_router.post("/contact-info-update/{userid}")
-async def update_contact_info():
-    pass
+@user_router.get(
+    "/by-role", response_model=list[PersonRead], status_code=status.HTTP_200_OK
+)
+async def get_users_by_role(
+    *,
+    session: Session = Depends(get_session),
+    role: Role,
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    # return role
+    return session.exec(
+        select(PersonDB).where(PersonDB.role == role).offset(offset).limit(limit)
+    )
+
+
+@user_router.get(
+    "/{person_id}", response_model=PersonRead, status_code=status.HTTP_200_OK
+)
+async def get_one_user(
+    *,
+    session: Session = Depends(get_session),
+    person_id: int,
+):
+    if person := session.get(PersonDB, person_id):
+        return person
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@user_router.post(
+    "/add", response_model=PersonRead, status_code=status.HTTP_201_CREATED
+)
+async def create_a_user(
+    *, session: Session = Depends(get_session), new_person: PersonCreate, role: Role
+):
+    hashed_password = hash_password(new_person.password)
+
+    extra_data = {
+        "password_hashed": hashed_password,
+        "role": role,
+        "username": create_default_username(
+            new_person.first_name, new_person.last_name
+        ),
+    }
+    db_person = PersonDB.model_validate(new_person, update=extra_data)
+    session.add(db_person)
+    session.commit()
+    session.refresh(db_person)
+    return db_person
+
+
+@user_router.patch("/update/{person_id}", response_model=PersonRead)
+async def update_a_user(
+    *,
+    session: Session = Depends(get_session),
+    person_id: int,
+    new_details: PersonUpdate,
+):
+    db_person = session.get(PersonDB, person_id)
+    if not db_person:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    update_data = new_details.model_dump(exclude_unset=True)
+    extra_data = {}
+    if "password" in update_data:
+        password = update_data["password"]
+        hashed_password = hash_password(password)
+        extra_data["hashed_password"] = hashed_password
+
+    if "username" in update_data:
+        lowercased = update_data["username"].lower()
+        extra_data["username"] = lowercased
+
+    db_person.sqlmodel_update(update_data, update=extra_data)
+    session.add(db_person)
+    session.commit()
+    session.refresh(db_person)
+    return db_person
+
+
+@user_router.delete("/delete/{person_id}")
+async def delete_user(
+    *,
+    session: Session = Depends(get_session),
+    person_id: int,
+):
+    person = session.get(PersonDB, person_id)
+    if not person:
+        raise HTTPException(status_code=404, detail="Person not found")
+    session.delete(person)
+    session.commit()
+    return {"deleted": True}
